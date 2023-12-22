67c758e319781a7c23f93a1f76e82aebd6ef099c
trieazon <trieason@gmail.com>
2023-12-22 14:43:40 +0000

    fixed backend api calls and refactored code to use async/await

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,24 @@
 import express from 'express';
 import axios from 'axios';
-// import products from './data/products.js';
 
 const app = express();
     
-app.get('/api/products', (request, response) => {
+app.get('/api/products', async (req, res) => {
     
-    axios
-        .get('https://raw.githubusercontent.com/Ovi/DummyJSON/master/src/data/products.json')
-        .then((res) => {
-            response.json(res.data);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+    const {data} = await axios.get(
+            'https://raw.githubusercontent.com/Ovi/DummyJSON/master/src/data/products.json'
+            );
+            
+    res.json(data);
 });
 
-app.get('/api/product/:id', (request, response) => {
+app.get('/api/product/:id', async (req, res) => {
     
-    axios
-        .get(`https://dummyjson.com/products/${request.params.id}`)
-        .then((res) => {
-            response.json(res.data);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+    const {data} = await axios.get(
+        `https://dummyjson.com/products/${req.params.id}`
+        );
+        
+        res.json(data);
 });
-
-// app.get('/api/product/:id', (req, res) => {
-//     const product = products.find(p => p.id == req.params.id);
-// })
 
 app.listen(8080, console.log('Server is running on port 8080'));
