c880fd774a208feec16f67d82870557e1b57f4aa
TrieaZon <108979358+TrieaZon@users.noreply.github.com>
2024-01-15 03:24:55 -0800

    prepared mongodb connection for future use, set up env, and started productModel

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import axios from 'axios';
-import products from './data/products.js'
+import connectDB from './config/db.js';
+// import products from './data/products.js'
 
 const app = express();
+dotenv.config();
+connectDB();
     
 app.get('/api/products', async (req, res) => {
     
