da16c0db90c53c67a6c8f90fff0e80d69ac7a744
TrieaZon <108979358+TrieaZon@users.noreply.github.com>
2024-01-23 00:36:49 -0800

    set up and connected redux store

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -1,28 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import ProductCard from './ProductCard';
+import React from 'react';
 import { Col } from 'react-bootstrap';
-import axios from 'axios';
+import ProductCard from './ProductCard';
 
-const ProductList = () => {
+const ProductList = ({products}) => {
     
-    const [products, setProducts] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-  
-    useEffect(() => {
-        const fetchProducts = async () => {
-            const {data} = await axios.get('/api/products');
-            setProducts(data);
-            console.log(data)
-            setIsLoading(false); 
-      };
-      fetchProducts();
-    },[isLoading]);
-  
     const productsArray = products.map(p => {
         return (
-            <Col sm={12} md={6} lg={4} xl={3}>
+            <Col key={ p._id } sm={12} md={6} lg={4} xl={3}>
                 <ProductCard
-                key={ p._id }
                 id={ p._id }
                 image={ p.image }
                 title={ p.name }
@@ -33,15 +18,11 @@ const ProductList = () => {
         );
     });
 
-    if(isLoading) {
-        return <h1>Loading...</h1>
-    } else {
-        return (
-            <>
-                { productsArray }
-            </>
-        );    
-    };   
+    return (
+        <>
+            { productsArray }
+        </>
+    );     
 };
 
 export default ProductList;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ProductList from '../components/ProductList';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Row } from 'react-bootstrap';
+import ProductList from '../components/ProductList';
+import { listProducts } from '../actions/productActions';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+
 
 const HomeScreen = () => {
     
-    
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(listProducts())
+    },[dispatch])
+
+    const productList = useSelector((state) => state.productList)
+    const {loading, products, error} = productList
     
     return (
         <>
             <h1>Latest Products</h1>
-            <Row>
-                <ProductList />
-            </Row>
+            
+                {
+                    loading? (<Loader />) :
+                        error ? (<Message variant='danger'>{error}</Message>) :
+                        (<Row>
+                            <ProductList products={ products } />
+                        </Row>)
+                }
+                
+            
         </>    
     );
 };
